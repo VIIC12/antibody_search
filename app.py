e6c6d67901e4be57ae728c0fc24fcaba4f2a5271
Tom Schlegel <tom.schlegel@uni-leipzig.de>
2026-08-19 18:53:51 +0200

    Some more styling

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ def main():
     
     # Global page configuration - applies to all pages by default
     st.set_page_config(
-        page_title="ABHunter - Antibody Database Search",
+        page_title="ABHunter Database Search",
         page_icon=':material/vaccines:',
         layout="wide",
         initial_sidebar_state="expanded",
